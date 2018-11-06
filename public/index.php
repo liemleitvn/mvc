a9539764719fdd23c65dd1fa46f6bbfffacb45b3
liemleitvn <liemleitvn@gmail.com>
2018-11-06 20:37:37 +0700

    init

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phanxicopeter
+ * Date: 11/6/18
+ * Time: 5:07 PM
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+$kernel = new \App\Kernel();
+$kernel->load();
